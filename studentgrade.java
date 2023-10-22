47d482a34a6d3c803106404a394247e19b3b9ef5
Pratham Kumbhare <129245523+prathham75@users.noreply.github.com>
2023-10-22 23:14:18 +0530

    Add files via upload
    This is the student grade calculator using java and this was the 2nd task of internship

---
+++ studentgrade.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class studentgrade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("STUDENT GRADE CALCULATOR");
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        int totalMarks = 0;
+
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + i + " (out of 100): ");
+            int marks = scanner.nextInt();
+            totalMarks += marks;
+        }
+
+        double averagePercentage = (double) totalMarks / (numSubjects * 100) * 100;
+
+        System.out.println("\nResults:");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.printf("Average Percentage: %.2f%%\n", averagePercentage);
+        System.out.println("Grade: " + getGrade(averagePercentage));
+    }
+
+    public static String getGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return "A+";
+        } else if (averagePercentage >= 80) {
+            return "A";
+        } else if (averagePercentage >= 70) {
+            return "B";
+        } else if (averagePercentage >= 60) {
+            return "C";
+        } else if (averagePercentage >= 50) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+}
